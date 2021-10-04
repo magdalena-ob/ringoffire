c461252a1162af464d67bbb52dbe1f9f75ac5a99
magdalena-o <magdalena.obermayr@gmx.at>
2021-10-04 12:11:52 +0200

    choose player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -32,6 +32,9 @@ export class GameComponent implements OnInit {
       console.log('New card: ' + this.currentCard);
       console.log('Game is ', this.game);
 
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard);
         this.pickCardAnimation = false;
